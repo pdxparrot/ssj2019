8a49a33bf87e809ca0942fd70a4c58be68f84ba9
Shane Lillie <ignignokterr@gmail.com>
2019-06-01 10:44:46 -0700

    Game viewer is a static viewer for now

--- Assets/Scripts/ssj2019/Camera/GameViewer.cs
+++ Assets/Scripts/ssj2019/Camera/GameViewer.cs
@@ -3,7 +3,7 @@ using pdxpartyparrot.Game.Camera;
 
 namespace pdxpartyparrot.ssj2019.Camera
 {
-    public sealed class GameViewer : SideScrollerViewer, IPlayerViewer
+    public sealed class GameViewer : StaticViewer, IPlayerViewer
     {
         public Viewer Viewer => this;
     }
