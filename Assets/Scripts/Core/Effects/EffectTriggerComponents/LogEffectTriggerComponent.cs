6ee2ef0dc3eb32d3207100b85cafdb4dab7d8b8f
Shane Lillie <ignignokterr@gmail.com>
2019-06-01 18:55:55 -0700

    Debug log effect trigger component

---
+++ Assets/Scripts/Core/Effects/EffectTriggerComponents/LogEffectTriggerComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace pdxpartyparrot.Core.Effects.EffectTriggerComponents
+{
+    public class LogEffectTriggerComponent : EffectTriggerComponent
+    {
+        [SerializeField]
+        private string _logMessage;
+
+        public override bool WaitForComplete => false;
+
+        public override void OnStart()
+        {
+            Debug.Log(_logMessage);
+        }
+    }
+}
