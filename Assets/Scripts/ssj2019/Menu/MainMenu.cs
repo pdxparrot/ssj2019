c7b58e66e4eb06384a7306df65e57eabca8a3689
Shane Lillie <ignignokterr@gmail.com>
2019-06-01 17:04:58 -0700

    Pause menu
    Character select menu

--- Assets/Scripts/ssj2019/Menu/MainMenu.cs
+++ Assets/Scripts/ssj2019/Menu/MainMenu.cs
@@ -1,11 +1,21 @@
-﻿using pdxpartyparrot.Game.State;
+﻿using pdxpartyparrot.Game.Menu;
+
+using UnityEngine;
 
 namespace pdxpartyparrot.ssj2019.Menu
 {
     public sealed class MainMenu : Game.Menu.MainMenu
     {
+        [SerializeField]
+        private CharacterSelectMenu _characterSelectPanel;
+
 #region Event Handlers
         public override void OnStart()
+        {
+            Owner.PushPanel(_characterSelectPanel);
+        }
+
+        public void OnReady()
         {
 //            GameStateManager.Instance.StartLocal(GameManager.Instance.GameGameData.MainGameStatePrefab);
         }
