01632ead76992608dedddcc426805a508b5a2983
Shane Lillie <ignignokterr@gmail.com>
2019-08-14 12:40:33 -0700

    Restore delay effect trigger to its former glory and add a note to it

--- Assets/Scripts/Core/Effects/EffectTriggerComponents/DelayEffectTriggerComponent.cs
+++ Assets/Scripts/Core/Effects/EffectTriggerComponents/DelayEffectTriggerComponent.cs
@@ -1,42 +1,29 @@
 ﻿using pdxpartyparrot.Core.Time;
+using pdxpartyparrot.Core.Util;
 
 using UnityEngine;
 
 namespace pdxpartyparrot.Core.Effects.EffectTriggerComponents
 {
+    // this delay ignores pause
+    // for a delay that doesn't ignore pause, use TimerEffectTriggerComponent
     public class DelayEffectTriggerComponent : EffectTriggerComponent
     {
         [SerializeField]
         private float _seconds;
 
+        [SerializeField]
+        [ReadOnly]
+        private bool _isWaiting;
+
         public override bool WaitForComplete => true;
 
-        public override bool IsDone => !_timer.IsRunning;
-
-        private ITimer _timer;
-
-#region Unity Lifecycle
-        private void Awake()
-        {
-            _timer = TimeManager.Instance.AddTimer();
-        }
-
-        private void OnDestroy()
-        {
-            if(TimeManager.HasInstance) {
-                TimeManager.Instance.RemoveTimer(_timer);
-            }
-        }
-#endregion
+        public override bool IsDone => !_isWaiting;
 
         public override void OnStart()
         {
-            _timer.Start(_seconds);
-        }
-
-        public override void OnStop()
-        {
-            _timer.Stop();
+            _isWaiting = true;
+            TimeManager.Instance.RunAfterDelay(_seconds, () => _isWaiting = false);
         }
     }
 }
